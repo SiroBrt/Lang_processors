6fd8d0d0a75ac19671e61999d2cf02be1ba9e4f1
dubi du <100496683@alumnos.uc3m.es>
2025-04-24 14:59:30 +0200

    a

---
+++ trad4/test.c
@@ -0,0 +1,21 @@
+// comentario
+//@
+int a, b = 1, c;
+a = 1;
+
+//@
+
+foo(int a, int b) {
+  if (a == b) {
+    puts("cosa");
+  } else {
+    puts("nada");
+  }
+}
+foo1() { int a; }
+
+main() {
+  int a;
+  a = -a;
+  i = i + 1;
+}
