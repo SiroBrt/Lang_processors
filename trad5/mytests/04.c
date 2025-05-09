44f88ee5d0738a4b36a83a9841cacb169e81fb31
dubi du <100496683@alumnos.uc3m.es>
2025-05-09 19:09:44 +0200

    04

--- trad5/mytests/04.c
+++ trad5/mytests/04.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int a, b = 1, c;
-a = 1;
 
 foo(int a, int b) {
   if (a == b) {
     puts("cosa");
   } else {
     puts("nada");
+    return a - b;
   }
+  return a + b;
 }
 foo1() {
   printf("%d %d %d %d %d %d %d %d %d %d", 1 || 0, 1 && 1, !1, 1 != 2, 1 == 2,
@@ -19,6 +20,7 @@ foo1() {
 main() {
   int n;
   int i, j;
+  n = 2;
   while (i < n) {
     foo1();
     for (j = 0; j < i; j = j + 1) {
@@ -33,3 +35,5 @@ main() {
     }
   }
 }
+
+//@(main)
