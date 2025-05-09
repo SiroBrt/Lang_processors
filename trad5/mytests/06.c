0eb219682ecebfb6206d3cc990c83607db2b9985
ruestajorge <114150761+ruestajorge@users.noreply.github.com>
2025-05-09 19:14:58 +0200

    Add files via upload

---
+++ trad5/mytests/06.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main (){
+    int i;
+    int x;
+    x = 2;
+    i = x*2+8-3/((x-3)*2);
+    if (i*x-8+x+4+(7)>= (x/i+13)*((-1)*(-i/4*7))){
+        printf("%d\n", i);
+    }
+    else {
+        printf("%d\n", x);
+    }
+}
